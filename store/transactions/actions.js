b7e5509cdae4ca444c65c6eed9888db78d9e7f05
thasuka <casferreiraa@gmail.com>
2022-04-24 20:40:34 -0300

    24-04 projeto finalizado
    projeto desenvolvido durante curso introdução ao nuxt de Tiago Matos

---
+++ store/transactions/actions.js
@@ -0,0 +1,16 @@
+export default{
+
+  getTransactions(){
+
+    return this.$axios.$get('transactions?_expand=category')
+
+  },
+
+  addTransaction(context,data){
+
+     return this.$axios.$post("transactions", data)
+
+  }  
+
+
+}
